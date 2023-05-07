fa26587cf8d66608111f867eff37e6684af56907
Juan Manuel Fernandes dos Santos <juan.fernandes@10pines.com>
2023-05-07 15:19:51 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Desafío del café con leche
+
+[https://wiki.uqbar.org/wiki/articles/desafios-cafe-con-leche.html](https://wiki.uqbar.org/wiki/articles/desafios-cafe-con-leche.html)
+
+Estos ejercicios son totalmente opcionales y son más complicados de los que normalmente mandamos como TPs.
+
+Quienes logren cada uno de los desafíos va a tener un premio que puede ser un café con leche con medialunas (o algo de valor menor o igual) del buffet.
+
+# ¿Cómo aviso cuando lo tenga resuelto?
+
+Sobre este repo no tienen permiso de escritura, lo pueden clonar, pero no pueden pushear, así que cuando lleguen a una solución que creen que es correcta, manden mail al profe con su solución (ya sea en código, en un archivo, o idealmente en un repo de github que hayan [forkeado](http://aprendegit.com/fork-de-repositorios-para-que-sirve/) a partir de este).
+
+# Gente que ya los resolvió
+
+![](https://i.imgur.com/p2FKUPJ.png)
+
+## $
+
+- 
+
+## Fold del árbol binario
+
+- 
---
+++ src/Library.hs
@@ -0,0 +1,60 @@
+module Library where
+import PdePreludat hiding (foldr, sum, elem, all)
+import Prelude (foldr, sum, elem, all, Foldable(..))
+
+-- Desafío, implementar la función aplicación, que recibe una función y un valor y aplica el valor como parámetro a la función.
+-- El desafío es que hay que implementarlo sin escribir parámetros, ni usar lambdas ni definir funciones auxiliares.
+-- Por ej., estas definiciones NO valen:
+-- ($) funcion valor = funcion valor -- porque estan los parametros
+-- ($) = \funcion valor -> funcion valor -- porque esta usando una lambda
+-- aplicar funcion valor = funcion valor -- porque se esta definiendo una funcion auxiliar 
+-- ($) = aplicar
+-- Tampoco vale importarlo (ahora se está no importando al hacer hiding (($)))
+
+($) :: (a -> b) -> a -> b
+($) = implementame
+
+--------------------------------------------------------------------
+
+-- Desafio 2: Foldr para arbol B
+--
+-- En computación, un arbol binario es un árbol donde cada nodo puede tener 0, uno o dos hijos.
+
+data ArbolBinario a = ArbolBinario a (ArbolBinario a) (ArbolBinario a) | Vacio
+
+-- agregamos la función hoja también para poder escribir más fácilmente las hojas del arbol:
+hoja :: a -> ArbolBinario a
+hoja valor = ArbolBinario valor Vacio Vacio
+
+-- Entonces, si quisiesemos representar un arbol como este:
+--            "hola"
+--           /   |
+--       "soy" "el"
+--              /\
+--        "arbol" "binario"
+--                  /
+--               "."
+-- lo podríamos escribir como:
+holaSoyElArbol :: ArbolBinario String
+holaSoyElArbol = ArbolBinario "hola" (hoja "soy")
+                                     (ArbolBinario "el" (hoja "arbol")
+                                                        (ArbolBinario "binario" (hoja ".") Vacio))
+
+-- Algo que no vimos durante la cursada es que foldr en realidad es parte de una typeclass,
+-- Foldable, o sea que no solo las listas se pueden foldear.
+--
+-- Como en el PdePreludat limitamos el tipo a que solo admita listas para hacerlo más simple,
+-- en este ejercicio los imports están un poco tocados para traer el foldr que viene de haskell
+-- en vez del del PdePreludat.
+--
+-- El desafío es hacer una implementación del foldr para esta estructura que haga pasar los tests.
+--
+-- Y algo interesante es que al implementar el foldr algunas funciones (como sum, all, any y elem)
+-- las vamos a tener "gratis" para este tipo.
+
+-- Nota: hay 3 implementaciones validas para el foldr, cada una va a hacer que el arbol se recorra
+-- de una forma diferente: prefija, infija o postfija. En este desafío, les pedimos que lo recorran
+-- de forma prefija.
+
+instance Foldable ArbolBinario where
+  foldr = implementame
